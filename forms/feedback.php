d6bb481111aa1ef3b7f3b22a8675fbcf15782ea3
Abhinav Kumar <46753270+Avikr76@users.noreply.github.com>
2021-08-16 20:53:18 +0530

    Update feedback.php

--- forms/feedback.php
+++ forms/feedback.php
@@ -6,10 +6,10 @@ $message = $_POST['message'];
 
 if(!empty($name) || !empty($email) || !empty($subject) || !empty($message))
 {	
-	$dbhost = 'localhost';
-	$username = 'root';
-	$password = '';
-	$dbselect = "foodkrafts";
+	$dbhost = 'remotemysql.com';
+	$username = 'XZpIP1YSCx';
+	$password = 'wAf9woxPjB';
+	$dbselect = "XZpIP1YSCx";
 	
 	$conn = new mysqli($dbhost, $username, $password, $dbselect );
 
